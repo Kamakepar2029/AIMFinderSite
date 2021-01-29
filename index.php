96d0a147d2f2142d1c938732d8d16ec140bb715e
KamaKepar <kirill@kamakepar.ru>
2021-01-29 07:53:53 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,53 @@
+<meta name="viewport" content="width=device-width">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="/style.css">
+<link rel="stylesheet" href="/csslide.css">
+<div class="header con">
+<div class="menu__menu">
+<div class="menu">
+<a href="#">Главная</a>
+<a href="#about">О приложении</a>
+<a href="#download">Скачать</a>
+<a href="/readme.html">Установка</a>
+<a href="#gallery">Галерея</a>
+</div>
+</div>
+<div class="content">
+<div class="left">
+<div class="title">TAFC</div>
+<div class="description">TAFC - TargetFuelCapture, программа для наложения прицела на камере в различных размерах.</div>
+</div>
+<div class="right">
+<img src="/cam.jpg" style="width:100%;box-shadow: 0px 0px 5px 4px #91bdd9;">
+</div>
+</div>
+</div>
+
+<div class="about con margin" id="about">
+<div class="title">О программе</div>
+<div class="description">Программа TAFC была написана за 1.5 дня. Она выполняет свою задачу на 100%.</div>
+</div>
+
+<div class="con margin" id="download">
+<div class="title">Скачать программу</div>
+<div class="description">Программа доступна для Windows</div>
+<div class="download">
+<a href="https://github.com/Kamakepar2029/tafc/releases/download/2.2-Beta/tafc-windows.zip" class="download-btn"><i class="fa fa-windows" aria-hidden="true"></i> Tafc Windows (58.8 MB)</a>
+</div>
+</div>
+<div class="gallery con margin" id="gallery">
+<div class="space" id="space">
+<div class="cssSlider">
+	<ul class="slides">
+  <li id="slide1"><img src="/cam.jpg" alt="" /></li>
+  <li id="slide2"><img src="/panel.jpg" alt="" /></li>
+  <li id="slide3"><img src="/select.jpg" alt="" /></li>
+  </ul>
+  <ul class="thumbnails">
+  <li><a href="#slide1"><img src="/cam.jpg" /></a></li>
+  <li><a href="#slide2"><img src="/panel.jpg" /></a></li>
+  <li><a href="#slide3"><img src="/select.jpg" /></a></li>
+  </ul>
+</div>
+</div>
+</div>
